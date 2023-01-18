493250180490c56370e966e6ff289b98c8a7fa69
Dioscar <dioscarr@gmail.com>
2023-01-18 18:14:38 -0500

    Added a bunch new enpoint

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import requests
+from urllib.parse import urlparse
+from validate_email import validate_email
 from bs4 import BeautifulSoup
 from urllib.parse import unquote
 from flask import Flask, jsonify
 from flask import request
+import re
 import time
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')  
 def hello():
@@ -21,7 +26,7 @@ def hello():
             return a['href']
     return "N/A"
     
-@app.route('/performance')
+@app.route('/performance' , methods=['GET'])
 def check_performance():
     website = request.args.get('url')    
     start_time = time.time()
@@ -30,5 +35,63 @@ def check_performance():
     total_time = end_time - start_time
     return jsonify({"website": website, "response_time": total_time})
 
+@app.route('/findemail', methods=['GET'])
+def create_map():
+    retry_count = 0
+    start_url = request.args.get('url')
+    visited_urls = set()
+    queue = [start_url]
+    email_addresses = set()
+    session = requests.Session()
+    parsed_start_url = urlparse(start_url)
+    while queue:
+        retry_count = retry_count + 1
+        url = queue.pop(0)
+        visited_urls.add(url)
+        try:
+            response = session.get(str(url))
+            content_type = response.headers.get('content-type')
+            if content_type:
+                if 'charset=' in content_type:
+                    encoding = content_type.split('charset=')[-1]
+                    response.encoding = encoding
+            email_regex = r'mailto:(\S+)'
+            soup = BeautifulSoup(response.content, 'html.parser')
+            for link in soup.find_all('a', href=True):               
+                match = re.search(email_regex, link['href'])
+                href = link.get('href')
+                if match and len(link.get('href').split(':')[1]) < 50:
+                    try:
+                        if link.get('href').startswith('mailto:'):
+                            email_addresses.add(link.get('href').split(':')[1])
+                        parsed_href = urlparse(href)
+                        if href.startswith('mailto:'):
+                            email = href.replace('mailto:', '')
+                            is_valid = validate_email(email)
+                            if is_valid:
+                                print('Email is valid' + email)
+                                email_addresses.add(email)
+                            else:
+                                print('Email is invalid')
+                        elif parsed_href.netloc == parsed_start_url.netloc and href not in visited_urls:
+                            queue.append(href)
+                    except ValueError as e:
+                        print(f'Error: {e}')
+            page_text = soup.get_text()
+            emails = re.findall(r'[\w\.-]+@[\w\.-]+', page_text)
+            is_valid = validate_email(emails)
+            if is_valid:
+                for email in emails:
+                    if is_valid:
+                        print('Email is valid' + email)
+                        email_addresses.add(email)
+                    else:
+                        print('Email is invalid')
+        except Exception as e:
+            print(f'Error: {e}')
+            if retry_count > 2:
+                return jsonify({"url":start_url,"emails":list(email_addresses)})
+    return jsonify({"url":start_url,"emails":list(email_addresses)})
+    
 if __name__ == '__main__':
     app.run(debug=True)
