286ef96742eb98e6820b3d1d51f2ac289ccabb16
Dioscar <dioscarr@gmail.com>
2023-02-06 21:25:01 -0500

    ...

--- app.py
+++ app.py
@@ -20,12 +20,18 @@ def hello():
     response = requests.get(str(url))
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    for span in soup.find_all('span'):
-        if 'icon--24-external-link-v2' in span['class']:
+    for span in soup.find_all('span',attrs={"class":"icon--24-external-link-v2" }):
+        #if 'icon--24-external-link-v2' in span['class']:
             parent = span.find_parent('a', href=True)
             if parent:
                 print(parent['href'])
                 return parent['href']
+    # for span in soup.find_all('span'):
+    #     if 'icon--24-external-link-v2' in span['class']:
+    #         parent = span.find_parent('a', href=True)
+    #         if parent:
+    #             print(parent['href'])
+    #             return parent['href']
     # for a in soup.find_all('a', href=True):
     #     if a.get_text().startswith('http'):
     #         print(a['href'])
@@ -66,38 +72,18 @@ def create_map():
             for link in soup.find_all('a', href=True):               
                 match = re.search(email_regex, link['href'])
                 href = link.get('href')
-                if match and len(link.get('href').split(':')[1]) < 50:
+                if match and len(link.get('href').split(':')[1]) < 90:
                     try:
-                        if link.get('href').startswith('mailto:'):
-                            email_addresses.add(link.get('href').split(':')[1])
-                        parsed_href = urlparse(href)
-                        if href.startswith('mailto:'):
-                            email = href.replace('mailto:', '')
-                            is_valid = validate_email(email)
-                            if is_valid:
-                                print('Email is valid' + email)
-                                email_addresses.add(email)
-                            else:
-                                print('Email is invalid')
-                        elif parsed_href.netloc == parsed_start_url.netloc and href not in visited_urls:
-                            queue.append(href)
+                        if link.get('href').startswith('mailto:'):        
+                            email_addresses.add(link.get('href').split(':')[1])                                             
+                            return jsonify({"url":start_url,"emails":list(email_addresses)})
                     except ValueError as e:
-                        print(f'Error: {e}')
-            page_text = soup.get_text()
-            emails = re.findall(r'[\w\.-]+@[\w\.-]+', page_text)
-            is_valid = validate_email(emails)
-            if is_valid:
-                for email in emails:
-                    if is_valid:
-                        print('Email is valid' + email)
-                        email_addresses.add(email)
-                    else:
-                        print('Email is invalid')
+                        email_addresses.add("N/A")
+                        return jsonify({"url":start_url,"emails":list(email_addresses)}) 
         except Exception as e:
-            print(f'Error: {e}')
-            if retry_count > 2:
-                return jsonify({"url":start_url,"emails":list(email_addresses)})
-    return jsonify({"url":start_url,"emails":list(email_addresses)})
-    
+            email_addresses.add("N/A")
+            return jsonify({"url":start_url,"emails":list(email_addresses)})       
+    email_addresses.add("N/A")        
+    return jsonify({"url":start_url,"emails":list(email_addresses)})    
 if __name__ == '__main__':
     app.run(debug=True)
